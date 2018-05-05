7c64eecd6c71f4666b99f760c140ac588caafbe1
Skylar Ittner <admin@netsyms.com>
2018-05-05 01:25:36 -0600

    Add contact form template to Identity theme

--- public/themes/identity/contact.php
+++ public/themes/identity/contact.php
@@ -1,6 +1,11 @@
+<?php
+include __DIR__ . "/inc/header.inc.php";
+?>
 <hr />
-<h2>Extra Stuff!</h2>
-<form method="post" action="#">
+<h2><div class="sw-text" data-component="contact-header">
+        <?php get_component("contact-header"); ?>
+    </div></h2>
+<form method="post" action="contact.php">
     <div class="field">
         <input type="text" name="name" id="name" placeholder="Name" />
     </div>
@@ -8,28 +13,22 @@
         <input type="email" name="email" id="email" placeholder="Email" />
     </div>
     <div class="field">
-        <div class="select-wrapper">
-            <select name="department" id="department">
-                <option value="">Department</option>
-                <option value="sales">Sales</option>
-                <option value="tech">Tech Support</option>
-                <option value="null">/dev/null</option>
-            </select>
-        </div>
-    </div>
-    <div class="field">
         <textarea name="message" id="message" placeholder="Message" rows="4"></textarea>
     </div>
-    <div class="field">
-        <input type="checkbox" id="human" name="human" /><label for="human">I'm a human</label>
-    </div>
-    <div class="field">
-        <label>But are you a robot?</label>
-        <input type="radio" id="robot_yes" name="robot" /><label for="robot_yes">Yes</label>
-        <input type="radio" id="robot_no" name="robot" /><label for="robot_no">No</label>
-    </div>
     <ul class="actions">
-        <li><a href="#" class="button">Get Started</a></li>
+        <li>
+            <?php
+            $btn = get_complex_component("submit-btn", null, ["link"]);
+            $icon = $btn['icon'];
+            $text = $btn['text'];
+            ?>
+            <button type="submit" class="button sw-complex" data-json="<?php echo get_escaped_json($btn); ?>" data-component="submit-btn">
+                <i class="<?php echo $icon; ?>"></i> <?php echo $text; ?>
+            </button>
+        </li>
     </ul>
 </form>
 <hr />
+<?php
+include __DIR__ . "/inc/footer.inc.php";
+?>
