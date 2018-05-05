394bee4d0e366d2e3959ea59251cbc489adfd7f2
Skylar Ittner <admin@netsyms.com>
2018-05-04 18:38:45 -0600

    Add social and company info/links to Verti footer

--- public/themes/verti/inc/footer.inc.php
+++ public/themes/verti/inc/footer.inc.php
@@ -5,7 +5,7 @@
             <div class="3u 6u(medium) 12u$(small)">
 
                 <!-- Links -->
-                <section class="widget links">
+                <!--<section class="widget links">
                     <h3>Random Stuff</h3>
                     <ul class="style2">
                         <li><a href="#">Etiam feugiat condimentum</a></li>
@@ -14,13 +14,13 @@
                         <li><a href="#">Felis varius pellentesque potenti</a></li>
                         <li><a href="#">Nullam scelerisque blandit leo</a></li>
                     </ul>
-                </section>
+                </section>-->
 
             </div>
             <div class="3u 6u$(medium) 12u$(small)">
 
                 <!-- Links -->
-                <section class="widget links">
+                <!--<section class="widget links">
                     <h3>Random Stuff</h3>
                     <ul class="style2">
                         <li><a href="#">Etiam feugiat condimentum</a></li>
@@ -29,13 +29,13 @@
                         <li><a href="#">Felis varius pellentesque potenti</a></li>
                         <li><a href="#">Nullam scelerisque blandit leo</a></li>
                     </ul>
-                </section>
+                </section>-->
 
             </div>
             <div class="3u 6u(medium) 12u$(small)">
 
                 <!-- Links -->
-                <section class="widget links">
+                <!--<section class="widget links">
                     <h3>Random Stuff</h3>
                     <ul class="style2">
                         <li><a href="#">Etiam feugiat condimentum</a></li>
@@ -44,7 +44,7 @@
                         <li><a href="#">Felis varius pellentesque potenti</a></li>
                         <li><a href="#">Nullam scelerisque blandit leo</a></li>
                     </ul>
-                </section>
+                </section>-->
 
             </div>
             <div class="3u 6u$(medium) 12u$(small)">
@@ -53,15 +53,27 @@
                 <section class="widget contact">
                     <h3>Contact Us</h3>
                     <ul>
-                        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-                        <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-                        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-                        <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-                        <li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+                        <?php
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-facebook"><span class="label">Facebook</span></a></li>', get_setting("facebook"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-twitter"><span class="label">Twitter</span></a></li>', get_setting("twitter"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-youtube"><span class="label">YouTube</span></a></li>', get_setting("youtube"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-instagram"><span class="label">Instagram</span></a></li>', get_setting("instagram"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-snapchat"><span class="label">Snapchat</span></a></li>', get_setting("snapchat"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-google-plus"><span class="label">Google+</span></a></li>', get_setting("google-plus"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-skype"><span class="label">Skype</span></a></li>', get_setting("skype"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-telegram"><span class="label">Twitter</span></a></li>', get_setting("telegram"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-vimeo"><span class="label">Vimeo</span></a></li>', get_setting("vimeo"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-whatsapp"><span class="label">Whatsapp</span></a></li>', get_setting("whatsapp"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>', get_setting("linkedin"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-asterisk"><span class="label">diaspora*</span></a></li>', get_setting("diaspora"));
+                        output_conditional('<li><a href="[[VAR]]" class="icon fa-mastodon"><span class="label">Mastodon</span></a></li>', get_setting("mastodon"));
+                        ?>
                     </ul>
-                    <p>1234 Fictional Road<br />
-                        Nashville, TN 00000<br />
-                        (800) 555-0000</p>
+                    <p>
+                        <?php output_conditional('<div style="display: flex;"><div><i class="fa fa-phone fa-fw"></i></div> <div style="font-size: 130%;">[[VAR]]</div></div>', get_setting("phone")); ?>
+                        <?php output_conditional('<div style="display: flex;"><div><i class="fa fa-map-marker fa-fw"></i></div> <div>[[VAR]]</div></div>', str_replace("\n", "<br />\n", get_setting("address"))); ?>
+                        <?php output_conditional('<div style="display: flex;"><div><i class="fa fa-envelope fa-fw"></i></div> <div>[[VAR]]</div></div>', get_setting("email")); ?>
+                        </p>
                 </section>
 
             </div>
@@ -70,7 +82,8 @@
             <div class="12u">
                 <div id="copyright">
                     <ul class="menu">
-                        <li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <?php output_conditional("<li>&copy; [[VAR]].  All rights reserved.</li>", get_setting("businessname")); ?>
+                        <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
                     </ul>
                 </div>
             </div>
