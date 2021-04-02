7b6ec52d5e6ec69f6a7e025a313820129ac8c7e6
Skylar Ittner <skylar@netsyms.com>
2021-04-02 15:31:39 -0600

    Add Content-Disposition filename header

--- public/file.php
+++ public/file.php
@@ -56,6 +56,7 @@ $ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
 header("Expires: $ts");
 header("Pragma: cache");
 header("Cache-Control: max-age=$seconds_to_cache");
+header("Content-Disposition: filename=\"" . pathinfo($filepath)['basename'] . "\"");
 
 ob_end_flush();
 
